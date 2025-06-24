44dbe46fad71466b2d52ed2bba75f047b247da76
Chala Temesgen <ctemesgen85@gmail.com>
2025-06-24 02:44:59 -0300

    Set Up NextAuth.js

---
+++ lib/auth.ts
@@ -0,0 +1,37 @@
+// lib/auth.ts
+import type { AuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import prisma from './prisma'
+import { compare } from 'bcrypt'
+
+const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        })
+
+        if (!user || !(await compare(credentials!.password, user.password))) {
+          throw new Error('Invalid credentials')
+        }
+
+        return { id: user.id + '', email: user.email, name: user.name ?? '' }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: 'jwt' as const,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+}
+
+export default authOptions
